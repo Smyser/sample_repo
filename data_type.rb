a3d4c6ac734a274e4dde03d5af2e792ac78cd0b2
Ezra Smyser <esmyser@gmail.com>
2013-02-02 13:55:21 -0500

    updated data types

---
+++ data_type.rb
@@ -0,0 +1,25 @@
+#set variable names
+#a is 1 (integer)
+#b is 2 (integer)
+#c is 3 (string)
+a = 1
+b = 2
+c = '3'
+
+
+#print a+b
+puts a + b
+
+
+#print a-b
+puts a - b
+
+#print 3 times a
+puts 3 * a
+
+#print 3 times c
+puts 3 * c
+
+#print c/b
+puts c / b
+
